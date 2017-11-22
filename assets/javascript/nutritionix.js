e738e5baa4ad2b8815d2a684cab748f32512e683
Chas Campbell <chas@chascampbell.com>
2017-11-22 14:56:52 -0500

    preparing to work on the menu creationsection

--- assets/javascript/nutritionix.js
+++ assets/javascript/nutritionix.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     // Variables for ingredient request.
     var ingredient = "";
     var results = {};
-    var calories = 0;
+    var itemCalories = 0;
     var itemName = "";
     var brandName = "";
     var servingSizeQty = 1;
@@ -54,10 +54,10 @@ $(document).ready(function() {
     // Variables for lookup.
     var lookupIngredient = "";
     var lookupMenu = "";
-    var lookupIngredientQty;
-    var lookupIngredientUnits = "";
-    var lookupIngredientCalories;
-    var lookupMenuCalories;
+    var lookupQty;
+    var lookupUnits = "";
+    var lookupCalories;
+    var lookupTotalCalories;
     var snapshot = {};
 
     // _________________________________________________REQUEST INPUT_
@@ -100,12 +100,12 @@ $(document).ready(function() {
                 for (var i = 0; i < results.hits.length; i++) {
                     itemName = results.hits[i].fields.item_name;
                     brandName = results.hits[i].fields.brand_name;
-                    calories = results.hits[i].fields.nf_calories;
+                    itemCalories = results.hits[i].fields.nf_calories;
                     servingSizeQty = results.hits[i].fields.nf_serving_size_qty;
                     servingSizeUnits = results.hits[i].fields.nf_serving_size_unit;
                     // Put the results into the html.
                     $("#info-table > tbody").append("<tr><td>" + itemName + "</td><td>" + brandName + "</td><td>" +
-                        calories + "</td><td>" + servingSizeQty + ' ' + servingSizeUnits + "</td></tr>");
+                        itemCalories + "</td><td>" + servingSizeQty + ' ' + servingSizeUnits + "</td></tr>");
                 } // End of for.
 
             }); // End of .done.
@@ -132,42 +132,59 @@ $(document).ready(function() {
         $("#qtyIn").val("");
         $("#unitsIn").val("");
         $("#caloriesIn").val("");
-
+        // Variable to hold the values for upload to Firebase.
         var savedObject = {
             "name": savedIngredient,
             "quantity": savedQty,
             "units": savedUnits,
             "calories": savedCalories
-        }; //End of savedObjewct.
+        }; //End of savedObject.
         console.log(savedObject);
-        console.log(savedQty);
-        console.log(savedUnits);
-        console.log(savedCalories);
         // Upload the saved ingredient to the database
         database.ref().push(savedObject);
-    }); // End of .on(click) for dataButton.
-    // ________________________________________________LOOKUP BUTTON
-    //Provide a listener for a lookup'
-    $("#LookupButton").on("click", function(event) {
+    }); // End of .on(click) for saveButton.
+    // ____________________________________________LOOKUP BUTTONS___________
+    //Provide a listener to look up an ingredient.
+    $("#lookupButtonOne").on("click", function(event) {
         event.preventDefault();
-        // Save values in variables.
-        lookupIngredient = $("#ingredientLookupLine").val().trim();
-        lookupMenu = $("#menuLookupLine").val().trim();
+        // Save value in variable.
+        lookupIngredient = $("#ingredientLookup").val().trim();
         //Get the related information.
         database.ref().orderByChild('name').equalTo(lookupIngredient).on("value", function(snapshot) {
-            console.log(snapshot.val());
-            // if (lookupIngredient != null) {
-            //     lookupIngredientQty = snapshot.ingredientSaved; //ingredientName.quantity;
-            //     //    lookupIngredientUnits = snapshot.ingredientSaved.ingredientName.units;
-            //     //    lookupIngredientCalories = snapshot.ingredientSaved.ingredientName.calories;
-            //     console.log(snapshot.chiid_added);
-            // }
-            // else if (lookupMenu != null) {
-            //     lookupMenuCalories = snapshot.Saved.menuName.calories;
-            // }
+            var savedShot = snapshot.val();
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                // Save the data in variables.
+                lookupQty = childData.quantity;
+                lookupUnits = childData.units;
+                lookupCalories = childData.calories;
+                // Load html with the variables.
+                $("#insertlIQ").html(lookupQty);
+                $("#insertlIU").html(lookupUnits);
+                $("#insertlIC").html(lookupCalories);
+            }); // End of snapshot forEach.
         }); // End of database.ref
-    }); // End of .on(click) for lookupButton.
-    // ________________________________________________CLEAR LOOKUP_______
+    }); // End of .on(click) for lookupButton1 for an ingredient.
+
+    //Provide a listener to look up a menu item.
+    $("#lookupButton2").on("click", function(event) {
+        event.preventDefault();
+        // Save value in variable.
+        lookupMenu = $("#menuLookup").val().trim();
+        //Get the related information.
+        database.ref().orderByChild('itemName').equalTo(lookupMenu).on("value", function(snapshot) {
+            console.log(snapshot.val());
+            var savedShot = snapshot.val();
+            snapshot.forEach(function(childSnapshot) {
+                console.log(childSnapshot.val());
+                var childData = childSnapshot.val();
+                // Save the data in variables.
+                lookupTotalCalories = childData.totalCalories;
+            }); // End of snapshot forEach.
+            // Save the data in variables.
+        });
+    });
+    // ____________________________________________CLEAR LOOKUP________________
     //Provide a listener to clear the lookup form.
     $("#clearButton3").on("click", function(event) {
         event.preventDefault();
@@ -193,45 +210,33 @@ $(document).ready(function() {
         sideTwoQty = $("#qtyTwoIn").val().trim();
         sideTwoUnits = $("#unitsTwoIn").val().trim();
 
-        // Get the database information for these items.
-        database.ref().on("value", function(snapshot) {
-            console.log(snapshot.val());
-            // Save the database values to variables.
-            // dbmainQty = snapshot.savedIngredient.ingredientName;
-            // dbsideOneQty = snapshot.savedIngredient.quantity;
-            // dbsideTwoQty = snapshot.savedIngredient.units;
-            // dbmainUnits = snapshot.savedIngredient.calories;
-            // dbsideOneUnits = snapshot.savedIngredient.;
-            // dbsideTwoUnits = snapshot.savedIngredient.;
-            // dbmainCalories = snapshot.savedIngredient.;
-            // dbsideOneCalories = snapshot.savedIngredient.;
-            // dbsideTwoCalories = snapshot.savedIngredient.;
+            // Save the database values to a variable for upload.
+            var menuObject = {
+                "dinner": menuItem,
+                "main": mainCourse,
+                "mquantity": mainQty,
+                "munits": mainUnits,
+                "mcalories": mainCalories,
+                "side1": sideOne,
+                "s1quantity": sideOneQty,
+                "s1units":sideOneUnits,
+                "s1calories": sideOneCalories,
+                "side2": sideTwo,
+                "s2quantity": sideTwoQty,
+                "s2units":sideTwoUnits,
+                "s2calories": sideTwoCalories
+            }  // End of var menu Object.
+            console.log(menuObject);
+            
+    });  // End of .on(click) for create button.
+    // Upload menuObject to Firebase.
+            database.ref().push(menuObject);
         }, function(errorObject) {
             console.log("The read failed: " + errorObject.code);
         });
-        // Check for matching units.
-        //Calculate the calories for the itwms.
-        // if (mainUnits != dbmainUnits) {
-        //     //alert("Main Units must be " + mainUnits);
-        // }
-        // else {
-        mainCaloriesOut = dbmainCalories * mainQty / dbmainQty;
-        // }
-        // if (sideOneUnits != dbsideOneUnits) {
-        //     //alert("Side One Units must be " + sideOneUnits);
-        // }
-        // else {
-        sideOneCaloriesOut = dbsideOneCalories * sideOneQty / dbsideOneQty;
-        // }
-        // if (sideTwoUnits != dbsideTwoUnits) {
-        //     //alert("Side Two Units must be " + sideTwoUnits);
-        // }
-        // else {
-        sideTwoCaloriesOut = dbsideTwoCalories * sideTwoQty / dbsideTwoQty;
-        totalCalories = mainCaloriesOut + sideOneCaloriesOut + sideTwoCaloriesOut;
-        $("#insert").html(totalCalories);
-        //    }
-        // End of three if-else.
+        //    sideTwoCaloriesOut = dbsideTwoCalories * sideTwoQty / dbsideTwoQty;
+        //    totalCalories = mainCaloriesOut + sideOneCaloriesOut + sideTwoCaloriesOut;
+        //    $("#insert").html(totalCalories)
     });
     // End of on(click) create.
 
@@ -274,9 +279,40 @@ $(document).ready(function() {
             "courseTotalCalories": totalCalories
         };
         // End of var menuItemSaved.
-
+        
         // Upload menu item to the database
         database.ref().push(savedMenuItem);
     }); // End of .on(click).
 
 }); // End of document ready.
+
+//  STUFF THAT DID NOT WORK
+
+//    let arr = Object.entries(snapshot).map(e => Object.assign(e[1], { key: e[0] }));
+//    console.log(arr);
+
+// function snapshotToArray(snapshot) {
+//     var returnArr = [];
+//     snapshot.forEach(function(childSnapshot) {
+//         var item = childSnapshot.val();
+//         item.key = childSnapshot.key;
+//         console.log(childSnapshot.val());
+//         console.log(returnArr);
+//     });
+
+
+//    snapshot.forEach(function(data) {
+//        console.log(data.key);
+//        var pushKey = data.key;
+//        console.log(savedShot.name);
+
+
+            // dbmainQty = 
+            // dbsideOneQty = snapshot.savedIngredient.quantity;
+            // dbsideTwoQty = snapshot.savedIngredient.units;
+            // dbmainUnits = snapshot.savedIngredient.calories;
+            // dbsideOneUnits = snapshot.savedIngredient.;
+            // dbsideTwoUnits = snapshot.savedIngredient.;
+            // dbmainCalories = snapshot.savedIngredient.;
+            // dbsideOneCalories = snapshot.savedIngredient.;
+            // dbsideTwoCalories = snapshot.savedIngredient.;
